9e3d019953b35f8cf9318a2980ad934f99c1c623
rafaelbatistaroque <maranata_rafa@hotmail.com>
2019-08-26 02:23:51 -0400

    Criado entidades de dominio.

---
+++ SysColab/SysColab.Dominio/Entities/Salario.cs
@@ -0,0 +1,16 @@
+﻿using sysColab.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sysColab.Domain.Entities
+{
+    public class Salario
+    {
+        public double Remuneracao { get; private set; }
+        public DateTime DataAlteracao { get; private set; }
+        public AlteracaoSalarial Motivo { get; private set; }
+    }
+}

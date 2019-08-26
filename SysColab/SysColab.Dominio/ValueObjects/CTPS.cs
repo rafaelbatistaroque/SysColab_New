42a5a02e426954ae6d0b1ffe4bdf37fb94716b43
rafaelbatistaroque <maranata_rafa@hotmail.com>
2019-08-26 03:08:40 -0400

    Criado o ObjectValue CTPS e seus testes

---
+++ SysColab/SysColab.Dominio/ValueObjects/CTPS.cs
@@ -0,0 +1,33 @@
+﻿using sysColab.Domain.Enums;
+using System;
+
+namespace sysColab.Domain.ValueObjects
+{
+    public class CTPS
+    {
+        public int Numero { get; private set; }
+        public int Serie { get; private set; }
+        public string Uf { get; private set; }
+        public CTPS(string numero, string serie, string uf)
+        {
+            EEstadosBrasileirosSigla ctpsUf;
+            if (!int.TryParse(numero, out int ctpfNr))
+                throw new ApplicationException("O campo CTPS Nr só aceita números");
+            else if (!int.TryParse(serie, out int ctpfSerie))
+                throw new ApplicationException("O campo CTPS Série só aceita números");
+            else if (string.IsNullOrWhiteSpace(uf) || char.IsDigit(uf, 0) || !VerificarSeEhEstadoBrasileiro(uf))
+                throw new ApplicationException("O campo CTPS UF só aceita siglas de Estados Brasileiros");
+            else
+            {
+                Numero = ctpfNr;
+                Serie = ctpfSerie;
+                Uf = ctpsUf.ToString();
+            }
+            bool VerificarSeEhEstadoBrasileiro(string estado)
+            {
+                bool convertidoParaEnum = Enum.TryParse(estado, true, out ctpsUf);
+                return convertidoParaEnum;
+            }
+        }
+    }
+}
---
+++ SysColab/SysColab.Testes/Domínio.Test/ValueObjects/CTPSTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using sysColab.Domain.Enums;
+using sysColab.Domain.ValueObjects;
+
+namespace SysColab.Testes.Domínio.Test.ValueObjects
+{
+    [TestClass]
+    public class CTPSTest
+    {
+        CTPS ctps;
+        [TestInitialize]
+        public void Inicial()
+        {
+            ctps = new CTPS("12", "123", "RJ");
+        }
+        [TestMethod]
+        public void DevePassarSeParamentro_NumeroDaCTPS_EhDoTipoInteiro()
+        {
+            Assert.IsTrue(char.IsDigit(ctps.Numero.ToString(), 0));
+            Assert.IsNotNull(ctps.Numero);
+        }
+        [TestMethod]
+        public void DevePassarSeParamentro_SerieDaCTPS_EhDoTipoInteiro()
+        {
+            Assert.IsTrue(char.IsDigit(ctps.Serie.ToString(), 0));
+        }
+        [TestMethod]
+        public void DevePassarSeParamentro_UFDaCTPS_EhSiglaDeUFBasileira()
+        {
+            Assert.IsTrue(Enum.IsDefined(typeof(EEstadosBrasileirosSigla), ctps.Uf));
+        }
+    }
+}

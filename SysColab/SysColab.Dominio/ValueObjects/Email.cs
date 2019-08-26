d9548bb25a0cb1b3b3886b5f19825683c06cffd3
rafaelbatistaroque <maranata_rafa@hotmail.com>
2019-08-26 03:10:46 -0400

    Criado ValuesObjects de Email e Nome.

---
+++ SysColab/SysColab.Dominio/ValueObjects/Email.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sysColab.Domain.ValueObjects
+{
+    public class Email
+    {
+        public string NomeEmail { get; private set; }
+        public Email(string email)
+        {
+            //FAZER: Criar regras de validação para email.
+            NomeEmail = email;
+        }
+    }
+}
---
+++ SysColab/SysColab.Dominio/ValueObjects/Nome.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace sysColab.Domain.ValueObjects
+{
+    public class Nome
+    {
+        public string PrimeiroNome { get; private set; }
+        public string SobreNome { get; private set; }
+
+        public Nome(string primeiroNome, string sobreNome)
+        {
+            //FAZER: Criar regras de validação de código para nome e sobrenome
+            PrimeiroNome = primeiroNome;
+            SobreNome = sobreNome;
+        }
+        public override string ToString()
+        {
+            return $"{PrimeiroNome} {SobreNome}";
+        }
+    }
+}
